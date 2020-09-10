f1aa1786e9a6feabefb7317871496cd02af25100
manoj kumar saini <mksaini026@gmail.com>
2020-09-10 11:07:49 +0530

    Android React Native App 1st commit

--- FirstApp/TestTextInput.js
+++ FirstApp/TestTextInput.js
@@ -8,7 +8,11 @@ export default class TestTextInput extends Component {
       <View>
         <TextInput style={{borderWidth:1, borderColor:'red', margin:40}} placeholder ="Enter the Name" placeholderTextColor='red'> </TextInput>
         <TextInput style={{borderWidth:1, borderColor:'red', margin:40, borderRadius:10}}> </TextInput>
-        <TextInput style={{borderWidth:1, borderColor:'red', margin:30}} placeholder ="Enter the Number" underlineColorAndroid='transparent'> </TextInput>
+        <TextInput style={{borderWidth:1, borderColor:'red', margin:30}} placeholder ="Enter the Number" keybordType='number-pad'> </TextInput>
+        <TextInput style={{borderWidth:1, borderColor:'red', margin:40, borderRadius:10}} defaultValue='101'> </TextInput>
+        <TextInput style={{borderWidth:1, borderColor:'red', margin:10, borderRadius:10}}> </TextInput>
+        <TextInput style={{borderWidth:1, borderColor:'red', margin:10, borderRadius:10}} maxLength={5}> </TextInput>
+        <TextInput style={{borderWidth:1, borderColor:'red', margin:10, borderRadius:10}} defaultValue='Monika' editable={false}> </TextInput>
       </View>
     );
   }
